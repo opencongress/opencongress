c4d2228eddda1366116057c2eb574ff8f3aff639
agp <agp@opencongress.org>
2009-09-03 01:25:52 -0400

    wiki bill creation number fix

--- app/controllers/bill_controller.rb
+++ app/controllers/bill_controller.rb
@@ -637,7 +637,7 @@ private
       if @bill.has_wiki_link?
         @wiki_url = @bill.wiki_url
       elsif logged_in?
-        @wiki_create_url = "#{WIKI_URL}/Special:AddData/Bill?Bill[common_title]=#{CGI::escape(@bill.title_common[0..60])}&Bill[bill_number]=#{@bill.title_typenumber_only.gsub!(".","")}&Bill[chamber]=#{@bill.is_senate_bill? ? "U.S. Senate" : "U.S.%20House%20of%20Representatives"}&Bill[congress]=#{DEFAULT_CONGRESS}" #prolly should be rewritten as a post handled by a custom sfEditFormPreloadText call?
+        @wiki_create_url = "#{WIKI_URL}/Special:AddData/Bill?Bill[common_title]=#{CGI::escape(@bill.title_common[0..60])}&Bill[bill_number]=#{@bill.bill_type}#{@bill.number}&Bill[chamber]=#{@bill.is_senate_bill? ? "U.S. Senate" : "U.S.%20House%20of%20Representatives"}&Bill[congress]=#{DEFAULT_CONGRESS}" #prolly should be rewritten as a post handled by a custom sfEditFormPreloadText call?
       else
         
       end

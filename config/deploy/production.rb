98a7e40253aead9a387ffeccc61a2419ad6b877d
Carl Tashian <carl-oc@tashian.com>
2009-11-16 16:05:23 -0800

    Don't update Cap current symlink until after linking images; run S3 sync after restarting server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,22 @@ role :app, "74.86.203.130"
 role :db,  "75.126.164.19", :primary => true
 role :db, "74.86.203.130"
 
+
+desc "Compress stylesheets and javascripts ahead of S3 sync"
+task :compress_static, :roles => :web, :only => {:asset_host_syncher => true} do
+  # static files in public/ are to be compressed ahead of time so they can sync with S3
+  # S3sync will deliver content-encoding headers for files ending .gz, .cssgz, or .jsgz
+  run "mkdir -p #{current_release}/public/min"
+  run "rm -rf #{current_release}/public/min/*"
+  run "cp -r #{current_release}/public/javascripts #{current_release}/public/min/js"
+  run "cp -r #{current_release}/public/stylesheets #{current_release}/public/min/css"
+  run "find #{current_release}/public/min -name *.[cj]s* -exec java -jar #{current_release}/bin/yuicompressor.jar {} -o {} \\;"
+  run "find #{current_release}/public/min -name *.[cj]s* -exec gzip {} \\;"
+end
+
+
 #
 # Sync with Amazon S3 asset hosts:
 #
-after "link_images", "s3_asset_host:synch_public"
+after "deploy:restart", "compress_static"
+after "deploy:restart", "s3_asset_host:synch_public"

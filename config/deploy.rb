6c8fada0a1ea5eb6661dde6ba11a03b5de8b3570
Carl Tashian <carl-oc@tashian.com>
2009-12-01 20:37:00 -0600

    Fix cap deploy:migrate

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ require 'capistrano/ext/multistage'
 #
 set :application, "opencongress"
 set :deploy_to, "/u/apps/opencongress"
+set :rake, "/opt/rubye/bin/rake"
 
 default_run_options[:pty] = true
 set :repository,  "git://github.com/opencongress/opencongress.git"
@@ -24,45 +25,32 @@ default_run_options[:pty] = true
 set :use_sudo, true
 
 desc "Link the images"
-task :link_images do
-  run "cp #{deploy_to}/#{shared_dir}/database.yml #{current_release}/config/database.yml"
-  run "cp #{deploy_to}/#{shared_dir}/api_keys.yml #{current_release}/config/api_keys.yml"
-  run "cp #{deploy_to}/#{shared_dir}/facebooker.yml #{current_release}/config/facebooker.yml"
-  run "cp #{deploy_to}/#{shared_dir}/settings.php #{current_release}/public/forum/conf/settings.php"
-  run "cp #{deploy_to}/#{shared_dir}/database.php #{current_release}/public/forum/conf/database.php"
-  run "ln -s #{deploy_to}/#{shared_dir}/states #{current_release}/public/images/states"
-  run "ln -s #{deploy_to}/#{shared_dir}/districts #{current_release}/public/images/districts"
-  run "ln -s #{deploy_to}/#{shared_dir}/user_images #{current_release}/public/images/users"
-  run "ln -s /data/govtrack/109/repstats/images/people #{current_release}/public/images/people"
-  run "ln -s /data/govtrack/photos #{current_release}/public/images/photos"
-  run "ln -s #{deploy_to}/#{shared_dir}/notebook_items #{current_release}/public/"
-	run "ln -s #{deploy_to}/#{shared_dir}/images #{current_release}/public/images/" 
-  run "ln -s #{deploy_to}/#{shared_dir}/files/oc_whats.flv #{current_release}/public/oc_whats.flv"
-  run "ln -s #{deploy_to}/#{shared_dir}/files/screencast.mp4 #{current_release}/public/screencast.mp4"
-  run "ln -s #{deploy_to}/#{shared_dir}/files/synch_s3_asset_host.yml #{current_release}/config/"
-  run "ln -s #{deploy_to}/#{shared_dir}/files/facebook.yml #{current_release}/config/"
-  sudo "chown -R mongrel:admins #{current_release}"
-  sudo "chmod 777 #{current_release}/public/forum/conf/settings.php"
-  sudo "chmod -R 777 #{current_release}/public/forum/extensions"
-end
-
-#
-# Delete all but the last 4 releases:
-#
-set :keep_releases, 4
-after "deploy:update", "deploy:cleanup"
 
 namespace :deploy do
 
-  task :after_update_code do
-    link_images
+  task :link_images do
+    run "cp #{deploy_to}/#{shared_dir}/database.yml #{current_release}/config/database.yml"
+    run "cp #{deploy_to}/#{shared_dir}/api_keys.yml #{current_release}/config/api_keys.yml"
+    run "cp #{deploy_to}/#{shared_dir}/facebooker.yml #{current_release}/config/facebooker.yml"
+    run "cp #{deploy_to}/#{shared_dir}/settings.php #{current_release}/public/forum/conf/settings.php"
+    run "cp #{deploy_to}/#{shared_dir}/database.php #{current_release}/public/forum/conf/database.php"
+    run "ln -s #{deploy_to}/#{shared_dir}/states #{current_release}/public/images/states"
+    run "ln -s #{deploy_to}/#{shared_dir}/districts #{current_release}/public/images/districts"
+    run "ln -s #{deploy_to}/#{shared_dir}/user_images #{current_release}/public/images/users"
+    run "ln -s /data/govtrack/109/repstats/images/people #{current_release}/public/images/people"
+    run "ln -s /data/govtrack/photos #{current_release}/public/images/photos"
+    run "ln -s #{deploy_to}/#{shared_dir}/notebook_items #{current_release}/public/"
+  	run "ln -s #{deploy_to}/#{shared_dir}/images #{current_release}/public/images/" 
+    run "ln -s #{deploy_to}/#{shared_dir}/files/oc_whats.flv #{current_release}/public/oc_whats.flv"
+    run "ln -s #{deploy_to}/#{shared_dir}/files/screencast.mp4 #{current_release}/public/screencast.mp4"
+    run "ln -s #{deploy_to}/#{shared_dir}/files/synch_s3_asset_host.yml #{current_release}/config/"
+    run "ln -s #{deploy_to}/#{shared_dir}/files/facebook.yml #{current_release}/config/"
+    sudo "chown -R mongrel:admins #{current_release}"
+    sudo "chmod 777 #{current_release}/public/forum/conf/settings.php"
+    sudo "chmod -R 777 #{current_release}/public/forum/extensions"
+  end
 
-    # If jammit is ever vendored, we'll need this:
-    #root_path    = File.expand_path(File.dirname(__FILE__) + '/..')
-    #jammit_path  = Dir["#{root_path}/vendor/gems/jammit-*/bin/jammit"].first
-    #yui_lib_path = Dir["#{root_path}/vendor/gems/yui-compressor-*/lib"].first
-    #assets_path  = "#{root_path}/public/assets"
-
+  task :jammit do
     # Compile CSS & JS for public/assets/ (see assets.yml)
     run "cd #{current_release}; /opt/rubye/bin/jammit"
   end
@@ -70,5 +58,14 @@ namespace :deploy do
   task :restart do
     sudo "touch #{deploy_to}/current/tmp/restart.txt"
   end
-
 end
+
+# Deploy hooks...
+
+#
+# Delete all but the last 4 releases:
+#
+set :keep_releases, 4
+after "deploy:update", "deploy:cleanup"
+after "deploy:update_code", "deploy:link_images"
+after "deploy:update_code", "deploy:jammit"

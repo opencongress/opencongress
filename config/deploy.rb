737082c49cdc3c1147d53a4fcb4f44fbb67654bb
Carl Tashian <carl-oc@tashian.com>
2009-12-04 12:12:45 -0600

    We don't need jammit --force after all

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ namespace :deploy do
 
   task :jammit do
     # Compile CSS & JS for public/assets/ (see assets.yml)
-    run "cd #{current_release}; /opt/rubye/bin/jammit --force"
+    run "cd #{current_release}; /opt/rubye/bin/jammit"
   end
 
   task :restart do

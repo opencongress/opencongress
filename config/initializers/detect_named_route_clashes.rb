24c072a453d0f20fbf08bece78bdf9550cc154ef
Carl Tashian <carl-oc@tashian.com>
2009-12-11 01:12:20 -0600

    Detect named route clashes when in development & test modes

---
+++ config/initializers/detect_named_route_clashes.rb
@@ -0,0 +1,22 @@
+module IGOpeople
+  module Routing
+    module BorkOnNamedRouteClash
+      def self.included(base)
+        base.alias_method_chain :add_named_route, :checking_clash
+      end
+
+      def add_named_route_with_checking_clash(name, path, options = {})
+        if named_routes[name.to_sym].nil?
+          add_named_route_without_checking_clash(name,path,options)
+        else
+          raise('clashing named route: '+ name.to_s)
+        end
+      end
+    end
+  end
+end
+
+if ['development','test'].include?(RAILS_ENV)
+  ActionController::Routing::RouteSet.send(:include, 
+      IGOpeople::Routing::BorkOnNamedRouteClash)
+end

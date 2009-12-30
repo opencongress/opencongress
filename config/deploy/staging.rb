e43063c9472c6dfb0dba8d6aac140751892c7637
Carl Tashian <carl-oc@tashian.com>
2009-12-30 12:39:10 -0600

    robots.txt for staging environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,3 +4,10 @@ set :deploy_to, "/u/apps/opencongress-dev"
 role :web, "dev.opencongress.org"
 role :app, "dev.opencongress.org"
 role :db,  "dev.opencongress.org", :primary => true
+
+namespace :deploy do
+  desc "Hook up robots.txt"
+  task :robots_txt do
+    run "ln -s #{deploy_to}/public/robots.txt.staging #{current_release}/public/robots.txt"
+  end
+end

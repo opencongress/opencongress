a44bbf1957b5d935ac5a4ecd84805b8f06c4c3b8
Carl Tashian <carl-oc@tashian.com>
2010-01-11 18:04:08 -0500

    First step to getting staging solr to persist between deployments.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,11 +7,30 @@ role :app, "dev.opencongress.org"
 role :db,  "dev.opencongress.org", :primary => true
 
 namespace :deploy do
-  desc "Hook up robots.txt"
-  task :robots_txt do
+  desc "Hook up staging symlinks"
+  task :symlinks do
     run "ln -s #{current_release}/public/robots.txt.staging #{current_release}/public/robots.txt"
+    run "mv #{current_release}/vendor/plugins/acts_as_solr/solr #{current_release}/vendor/plugins/acts_as_solr/solr-notused"
+    run "ln -s #{deploy_to}/#{shared_dir}/solr #{current_release}/vendor/plugins/acts_as_solr/solr"
   end
 end
 
 
-after "deploy:update_code", "deploy:robots_txt"
+after "deploy:update_code", "deploy:symlinks"
+
+# We can control solr on staging, just not on production because
+# for production it's running outside of Rails entirely.
+namespace :solr do
+  task :start, :roles => :app do
+      run "cd #{latest_release} && #{rake} solr:start RAILS_ENV=staging 2>/dev/null"
+  end
+
+  task :stop, :roles => :app do
+      run "cd #{latest_release} && #{rake} solr:stop RAILS_ENV=staging 2>/dev/null"
+  end
+
+  task :restart, :roles => :app do
+      solr.stop
+  solr.start
+  end
+end

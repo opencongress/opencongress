4da3b6d172536db2c4314c4ebf805b18fb0d4362
Andrew Ross <aross@rocknroll.lan>
2010-01-16 22:54:56 +0000

    reduced number of ajax calls

--- app/controllers/commentary_controller.rb
+++ app/controllers/commentary_controller.rb
@@ -21,4 +21,14 @@ class CommentaryController < ApplicationController
       render :text => "<font style='color:#45A307;'>You must be logged in to rate articles. Log in above.</font>"
     end
   end
+  
+  def pn_ajax_multiple
+    logger.warn "object_ids #{params[:object_ids]}"
+
+    commentary_ids = params[:object_ids].split(",")
+    @commentaries = Commentary.find(:all, :conditions => ["id IN (?)", commentary_ids])
+    respond_to do |format|
+      format.js
+    end   
+  end
 end

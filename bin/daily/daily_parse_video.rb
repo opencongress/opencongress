b5e9eaf37e9e026741dff7fc211e56c9ca776cb8
Andrew Ross <aross@rocknroll.local>
2010-01-25 17:22:44 +0100

    youtube changed link format

--- bin/daily/daily_parse_video.rb
+++ bin/daily/daily_parse_video.rb
@@ -84,14 +84,20 @@ def youtube_item_to_video(item, person = nil)
   puts "Got a youtube video: #{item.text('title')}"
   #puts "inspect: #{item}"
   vid_url = item.text('link')
+  yt_id = nil
   
   Video.transaction {
     v = Video.find_or_initialize_by_url(vid_url.strip)
     v.title = item.text('title')
     v.source = 'youtube'
     
-    # get youtube ID from the 
-    yt_id = v.url.split(/=/).last 
+    # get youtube ID from the
+    v.url.split(/\?/).last.split(/\&/).each do |kv|
+      key, val = kv.split(/\=/)
+      if key == 'v'
+        yt_id = val
+      end
+    end 
     
     v.embed = "<object width=\"425\" height=\"344\"><param name=\"movie\" value=\"http://www.youtube.com/v/#{yt_id}&hl=en&fs=1\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/#{yt_id}&hl=en&fs=1\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"425\" height=\"344\"></embed></object>" 
     v.video_date = Date.parse(item.text('pubDate'))

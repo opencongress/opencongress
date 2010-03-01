7adafbfb30a8b033dd25bd9e713963c3b44c0c5d
agp <agp@opencongress.org>
2010-03-01 18:15:55 -0500

    add index for bad commentaries

--- db/migrate/20100228211106_create_bad_commentaries.rb
+++ db/migrate/20100228211106_create_bad_commentaries.rb
@@ -2,6 +2,7 @@ class CreateBadCommentaries < ActiveRecord::Migration
   def self.up
       execute "CREATE TABLE bad_commentaries AS SELECT commentaries.url, commentaries.commentariable_id, commentaries.commentariable_type, commentaries.date FROM commentaries WHERE commentaries.is_ok = FALSE AND commentaries.status != 'PENDING'"
       execute "ALTER TABLE bad_commentaries ADD COLUMN id serial"
+      add_index :bad_commentaries, [:url, :commentariable_id, :commentariable_type]
       execute "DELETE FROM commentaries WHERE commentaries.is_ok = FALSE AND commentaries.status != 'PENDING'"
       execute "REINDEX table commentaries"
   end

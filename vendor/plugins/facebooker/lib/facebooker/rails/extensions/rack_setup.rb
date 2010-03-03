3b3844d059c8a7c75761acb3e858d2a11b444431
Carl Tashian <carl-oc@tashian.com>
2010-03-03 12:35:12 -0800

    Rolling on Rails 2.3.5

--- vendor/plugins/facebooker/lib/facebooker/rails/extensions/rack_setup.rb
+++ vendor/plugins/facebooker/lib/facebooker/rails/extensions/rack_setup.rb
@@ -1,2 +1,3 @@
 require 'rack/facebook'
-ActionController::Dispatcher.middleware.insert_after 'ActionController::RewindableInput',Rack::Facebook, Facebooker.secret_key
+ActionController::Dispatcher.middleware.insert_after(
+ (Object.const_get('ActionController::RewindableInput') rescue false) ? 'ActionController::RewindableInput' : 'ActionController::Session::CookieStore', Rack::Facebook, Facebooker.secret_key )

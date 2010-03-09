003c24522dff6c20c94f30786a48bcdd50e57fbe
Carl Tashian <carl-oc@tashian.com>
2010-03-09 14:54:39 -0800

    Homepage popular dropdown menu now closes on click

--- public/javascripts/frontpage.js
+++ public/javascripts/frontpage.js
@@ -1,9 +1,34 @@
+
+var onMenu = 0;
 function switchBillTab(to, title) {
     allTabs = Element.childElements('bill_tabs')
     allTabs.each(function(t) {
-       Element.hide(t)
+       Element.hide(t);
     });
     Element.show(to);
     $('popular_ul_title').innerHTML = title;
-
+    onMenu = 0;
+    $j('#bill_tab_select').hide().toggleClass('small_heading_hover');
 }
+
+$j(document).ready(function(){
+
+$j('#popular_ul_title').hover(function(event) {
+    $j('#bill_tab_select').show().toggleClass('small_heading_hover');
+}, function(event) {
+    if (onMenu == 0) {
+        $j('#bill_tab_select').hide().toggleClass('small_heading_hover');
+    }
+});
+
+$j('#bill_tab_select').hover(function(event) {
+    $j('#bill_tab_select').show().toggleClass('small_heading_hover');
+    onMenu = 1;
+}, function(event) {
+   if (onMenu == 1) {
+       onMenu = 0;
+       $j('#bill_tab_select').hide().toggleClass('small_heading_hover');
+   }
+});
+
+});

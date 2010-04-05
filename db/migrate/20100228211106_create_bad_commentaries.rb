9b8f46c2b5ebc113cd368df1606748f77204ded4
Carl Tashian <carl-oc@tashian.com>
2010-04-05 09:59:27 -0700

    Commentary migration: index name length bug workaround
    See https://rails.lighthouseapp.com/projects/8994/tickets/3252-add_index-on-postgresql-with-long-index-names-migrations-fail-with-input-string-is-longer-than-namedatalen-1-63 for details on this bug.

--- db/migrate/20100228211106_create_bad_commentaries.rb
+++ db/migrate/20100228211106_create_bad_commentaries.rb
@@ -4,13 +4,15 @@ class CreateBadCommentaries < ActiveRecord::Migration
       execute "ALTER TABLE bad_commentaries ADD COLUMN id serial"
       
       add_index :bad_commentaries, :url
-      add_index :bad_commentaries, [:commentariable_id, :commentariable_type]
+      add_index :bad_commentaries, [:commentariable_id, :commentariable_type], :name => 'bad_commentariable_idx'
       
       execute "DELETE FROM commentaries WHERE commentaries.is_ok = FALSE AND commentaries.status != 'PENDING'"
       execute "REINDEX table commentaries"
   end
 
   def self.down
+    remove_index :bad_commentaries, :name => 'bad_commentariable_idx'
+    remove_index :bad_commentaries, :url
     drop_table :bad_commentaries
   end
 end

e36cf688893a727aba9a84578ce610430a0189be
Carl Tashian <carl@ppolitics.org>
2010-06-16 16:34:36 -0700

    Link up newrelic.yml on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,7 @@ namespace :deploy do
   task :link_images do
     run "cp #{deploy_to}/#{shared_dir}/database.yml #{current_release}/config/database.yml"
     run "cp #{deploy_to}/#{shared_dir}/api_keys.yml #{current_release}/config/api_keys.yml"
+    run "cp #{deploy_to}/#{shared_dir}/newrelic.yml #{current_release}/config/newrelic.yml"
     run "cp #{deploy_to}/#{shared_dir}/facebooker.yml #{current_release}/config/facebooker.yml"
     run "ln -s #{deploy_to}/#{shared_dir}/states #{current_release}/public/images/states"
     run "ln -s #{deploy_to}/#{shared_dir}/user_images #{current_release}/public/images/users"

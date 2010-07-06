18b378a4f967281a5e082c16b71e1faedaf0fc27
Carl Tashian <carl@ppolitics.org>
2010-07-06 13:51:39 -0700

    Refactoring of CiviCRM sync

--- bin/civicrm_sync.rb
+++ bin/civicrm_sync.rb
@@ -47,7 +47,7 @@ end
 
 def get(query)
   # TODO: Use SSL for this.
-  puts "Opening " + 'http://crm.ppolitics.org/sites/all/modules/civicrm/extern/rest.php?q=civicrm' + query + AUTH_PART
+  Rails.logger.debug "Opening " + 'http://crm.ppolitics.org/sites/all/modules/civicrm/extern/rest.php?q=civicrm' + query + AUTH_PART
   doc = Hpricot(open('http://crm.ppolitics.org/sites/all/modules/civicrm/extern/rest.php?q=civicrm' + query + AUTH_PART))
 
   if !(doc/"is_error").empty? && (doc/"is_error").first.inner_html == "1"
@@ -62,44 +62,47 @@ def add_to_group(id)
   get("/group_contact/add&contact_id=#{id}&group_id=#{OC_CIVICRM_GROUP_ID}")
 end
 
+def add_contact(ops = {})
+  uri = ""
+  ops[:contact_type] = "Individual"
+  ops.each { |l, r| uri = uri + "&#{l}=#{es(r)}"}
+
+  return get("/contact/add" + ops)
+end
+
 # The main loop.
 UserAudit.all(:conditions => ["created_at > ?", last_sync_at]).each do |a|
   doc = get("/contact/search&email=#{es(a.email_was? ? a[:email_was] : a.email)}")
 
   contact_id = first_inner(doc, "contact_id")
 
-  # When someone needs to be created
-  create_part = "&first_name=#{es(a.full_name)}&email=#{es(a.email)}"
-  opt_out_part = "&is_opt_out=#{a.action == "subscribe" ? 0 : 1}"
-  type_part = "&contact_type=Individual"
-
   case a.action
   when 'subscribe', 'unsubscribe':
     # If someone is found:
     if contact_id = first_inner(doc, "contact_id")
-      puts "Updating opt out for contact id ##{contact_id} (#{a.email})"
-      get("/contact/add&contact_id=#{contact_id}" + opt_out_part + type_part)
+      Rails.logger.debug "Updating opt out for contact id ##{contact_id} (#{a.email})"
+      doc = add_contact :contact_id => contact_id, :is_opt_out => (a.action == "subscribe" ? 0 : 1)
     else
       # Create a new contact
       puts "Going to create a new contact for #{a.full_name}"
-      c = get(create_part + opt_out_part + type_part)
+      c = add_contact :first_name => a.full_name, :email => a.email, :is_opt_out => (a.action == "subscribe" ? 0 : 1)
       if new_id = first_inner(c, "contact_id")
         add_to_group new_id
       else
-        puts "Error: No contact ID for new contact"
+        Rails.logger.error "Error: No contact ID for new contact"
       end
     end
   when 'update':
     # If contact found:
     if contact_id = first_inner(doc, "contact_id")
-      puts "Updating CiviCRM contact ##{contact_id}"
-      get "/contact/add&contact_id=#{contact_id}&first_name=#{es(u.full_name)}&email=#{es(a.email)}"
+      Rails.logger.debug "Updating CiviCRM contact ##{contact_id}"
+      add_contact :contact_id => contact_id, :first_name => a.full_name, :email => a.email
     else
       # Create a new contact
-      puts "Creating a new contact and adding ato "
-      get create_part + type_part
+      Rails.logger.debug "Creating a new contact"
+      c = add_contact :first_name => a.full_name, :email => a.email
       if new_id = first_inner(c, "contact_id")
-        add_to_group contact_id
+        add_to_group new_id
       end
     end
   end

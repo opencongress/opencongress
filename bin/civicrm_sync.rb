e81a0db65c4ab5bf682f28f0e1b28fda65f1a68e
Carl Tashian <carl@ppolitics.org>
2010-07-07 10:28:51 -0700

    Update the zipcode and district for contacts

--- bin/civicrm_sync.rb
+++ bin/civicrm_sync.rb
@@ -79,11 +79,22 @@ UserAudit.all(:conditions => ["created_at >= ?", last_sync_at], :order => "creat
     # If someone is found:
     if contact_id = first_inner(doc, "contact_id")
       puts "Updating opt out for contact id ##{contact_id} (#{a.email})"
-      doc = add_contact :contact_id => contact_id, :is_opt_out => (a.action == "subscribe" ? 0 : 1)
+      doc = add_contact(
+        :contact_id => contact_id,
+        :is_opt_out => (a.action == "subscribe" ? 0 : 1)
+      )
     else
       # Create a new contact
       puts "Going to create a new contact for #{a.full_name}"
-      c = add_contact :first_name => a.full_name, :email => a.email, :is_opt_out => (a.action == "subscribe" ? 0 : 1)
+      c = add_contact(
+        :first_name => a.full_name,
+        "email[1][email]" => a.email,
+        "email[1][location_type_id]" => 1,
+        "address[1][location_type_id]" => 1,
+        "address[1][postal_code]" => a.zipcode,
+        :custom_1 => a.district,
+        :is_opt_out => (a.action == "subscribe" ? 0 : 1)
+      )
       if new_id = first_inner(c, "contact_id")
         add_to_group new_id
       else
@@ -94,11 +105,26 @@ UserAudit.all(:conditions => ["created_at >= ?", last_sync_at], :order => "creat
     # If contact found:
     if contact_id = first_inner(doc, "contact_id")
       puts "Updating CiviCRM contact ##{contact_id}"
-      add_contact :contact_id => contact_id, :first_name => a.full_name, :email => a.email
+      add_contact(
+        :contact_id => contact_id,
+        :first_name => a.full_name,
+        "email[1][email]" => a.email,
+        "email[1][location_type_id]" => 1,
+        "address[1][location_type_id]" => 1,
+        "address[1][postal_code]" => a.zipcode,
+        :custom_1 => a.district,
+      )
     else
       # Create a new contact
       puts "Creating a new contact"
-      c = add_contact :first_name => a.full_name, :email => a.email
+      c = add_contact(
+        :first_name => a.full_name,
+        "email[1][email]" => a.email,
+        "email[1][location_type_id]" => 1,
+        "address[1][location_type_id]" => 1,
+        "address[1][postal_code]" => a.zipcode,
+        :custom_1 => a.district,
+      )
       if new_id = first_inner(c, "contact_id")
         add_to_group new_id
       end

80f2bbe8acc1c831ab6faf9fae9fe44c3cff0a52
Andrew Ross <aross@opencongress.org>
2011-02-10 21:24:42 -0800

    added hoptoad for error tracking

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,7 @@ role :web, "75.126.164.19", :asset_host_syncher => true
 role :web, "74.86.203.130"
 role :app, "75.126.164.19"
 role :app, "74.86.203.130"
+role :app, "worker.opencongress.org"
 role :db,  "75.126.164.19", :primary => true
 role :db, "74.86.203.130"
 
@@ -12,3 +13,6 @@ role :db, "74.86.203.130"
 # Sync with Amazon S3 asset hosts:
 #
 before "deploy:symlink", "s3_asset_host:synch_public"
+
+
+

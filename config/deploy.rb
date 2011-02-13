77db919bf6ef5a26442c77c2a5604780e000e5f5
Andrew Ross <aross@opencongress.org>
2011-02-12 18:04:11 -0800

    added hoptoad notification to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,9 +74,9 @@ after "deploy:update_code", "deploy:link_images"
 after "deploy:update_code", "deploy:jammit"
 
 
-# HopToad install put this in.  Not sure we need it yet.
-# Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
-#   $: << File.join(vendored_notifier, 'lib')
-# end
-# 
-# require 'hoptoad_notifier/capistrano'
+# HopToad notification.
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'

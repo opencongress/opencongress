35459596583c9593e826accbf96409ca82fb8724
Andrew Ross <aross@opencongress.org>
2011-02-28 19:55:29 -0800

    fixed staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,31 +1,36 @@
-# Settings specified here will take precedence over those in config/environment.rb
+OpenCongress::Application.configure do
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+  # Disable Rails's static asset server
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = false
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = false
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
 
-# Disable delivery errors if you bad email addresses should just be ignored
-# config.action_mailer.raise_delivery_errors = false
+  # Enable threaded mode
+  # config.threadsafe!
 
-config.cache_store = :mem_cache_store, 'localhost:11211', { :namespace => 'opencongress_staging' }
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
 
-if defined?(PhusionPassenger)
-    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-        if forked
-            # We're in smart spawning mode.
-            Rails.cache.instance_variable_get(:@data).reset
-        else
-            # We're in conservative spawning mode. We don't need to do anything.
-        end
-    end
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+  
+  ## removed when switched to rails 3.  still necessary?? 
+  # if defined?(PhusionPassenger)
+  #     PhusionPassenger.on_event(:starting_worker_process) do |forked|
+  #         if forked
+  #             # We're in smart spawning mode.
+  #             Rails.cache.instance_variable_get(:@data).reset
+  #         else
+  #             # We're in conservative spawning mode. We don't need to do anything.
+  #         end
+  #     end
+  # end
 end
-
-MINI_MAILER_FROM = "alerts@dev.opencongress.org"
-

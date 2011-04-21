9148123728ea156e1d0735a4ae8d2111efd52bde
Andrew Ross <aross@opencongress.org>
2011-04-21 15:23:16 -0700

    bill/all redesign and some comment fixes

--- app/models/bill_text_node.rb
+++ app/models/bill_text_node.rb
@@ -23,10 +23,13 @@ class BillTextNode < ActiveRecord::Base
     
     path = "#{Settings.oc_billtext_path}/#{bill_text_version.bill.session}/#{bill_text_version.bill.bill_type}#{bill_text_version.bill.number}#{bill_text_version.version}.gen.html-oc"
     
-    doc = Nokogiri::XML(open(path))
+    begin
+      doc = Nokogiri::XML(open(path))    
+      node = doc.css("p[@id='bill_text_section_#{nid}']")
     
-    node = doc.css("p[@id='bill_text_section_#{nid}']")
-    
-    @bill_text_cache = node.text.gsub(/CommentsClose CommentsPermalink/, "")
+      @bill_text_cache = node.text.gsub(/CommentsClose CommentsPermalink/, "")
+    rescue
+      return ""
+    end
   end
 end

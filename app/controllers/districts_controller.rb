79141d7cbbba88833efe61ad0a6468f63bce16a5
tashian <carl@ppolitics.org>
2011-07-05 16:38:04 -0700

    District page updates -- and move people box into a partial

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -9,6 +9,11 @@ class DistrictsController < ApplicationController
   # GET /states/:state_id/districts/:id
   def show
     @district = @state.districts.find_by_district_number(params[:id])
+
+    @representative = Person.find_current_representative_by_state_and_district(@state.abbreviation, @district.district_number)
+
+    @senators = Person.find_current_senators_by_state(@state.abbreviation)
+
     @page_title = "#{@state.name.titleize}'s #{@district.ordinalized_number} Congressional District"
     @users = @district.users
     @tracking_suggestions = @district.tracking_suggestions

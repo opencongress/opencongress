431308fd53486f8ece9c934e4752c93edd148508
Andrew Ross <aross@opencongress.org>
2011-07-25 10:45:44 -0400

    more facebook debugging

--- config/initializers/facebooker2.rb
+++ config/initializers/facebooker2.rb
@@ -1,1 +1,13 @@
 Facebooker2.load_facebooker_yaml
+
+
+module Facebooker2
+  module Rails
+    module Controller
+      def fb_cookie_signature_correct?(hash,secret)
+        puts "hash: #{hash}, secret: #{secret}, generate_sig: #{generate_signature(hash,secret)}, hash[sig]: #{hash['sig']}"
+        generate_signature(hash,secret) == hash["sig"]
+      end
+    end
+  end
+end

dd38027dfc778169e8261bad633266065908d115
Andrew Ross <aross@opencongress.org>
2011-07-27 21:58:42 -0400

    updated formageddon fixed bug with incoming receiver

--- app/controllers/contact_congress_letters_controller.rb
+++ app/controllers/contact_congress_letters_controller.rb
@@ -220,23 +220,26 @@ class ContactCongressLettersController < ApplicationController
   end
   
   def get_replies
-    emails_received = 0
-    notifications_sent = 0
     if params[:formageddon_get_replies_key] and params[:formageddon_get_replies_key] == ApiKeys.formageddon_get_replies_key
+      emails_received = 0
+      notifications_sent = 0
+       
       Formageddon::IncomingEmailFetcher.fetch do |letter|
         cclft = ContactCongressLettersFormageddonThread.where(["formageddon_thread_id=?", letter.formageddon_thread.id]).first
 
         emails_received += 1
         
-        if cclft and cclft.receive_replies?
+        if cclft and cclft.contact_congress_letter.receive_replies?
           notifications_sent += 1
           Rails.logger.info "Sending an email notification to: #{cclft.contact_congress_letter.user.email}"
           ContactCongressMailer.reply_received_email(cclft.contact_congress_letter, letter.formageddon_thread).deliver
         end
       end
+      
+      render :text => "#{emails_received} emails, #{notifications_sent} notifications"
+    else
+      render :text => "Access denied."
     end
-    
-    render :text => "#{emails_received} emails, #{notifications_sent} notifications"
   end
   
   private

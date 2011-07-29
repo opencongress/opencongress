12c4fd648f97b842db2d6ffb0fb1be4b976ad0a3
Andrew Ross <aross@opencongress.org>
2011-07-29 09:58:41 -0400

    made script use curl

--- bin/incoming_contact_congress_emails.rb
+++ bin/incoming_contact_congress_emails.rb
@@ -1,19 +1,3 @@
-#!/usr/bin/env ruby
+#!/bin/sh
 
-#### LOAD RAILS ENVIRONMENT
-APP_PATH = File.expand_path('../../config/application',  __FILE__)
-require File.expand_path('../../config/boot',  __FILE__)
-ENV["RAILS_ENV"] ||= "development"
-require APP_PATH
-Rails.application.require_environment!
-###########################
-
-
-Formageddon::IncomingEmailFetcher.fetch do |letter|
-  cclft = ContactCongressLettersFormageddonThread.where(["formageddon_thread_id=?", letter.formageddon_thread.id]).first
-  
-  if cclft
-    puts "Sending an email notification to: #{cclft.contact_congress_letter.user.email}"
-    ContactCongressMailer.reply_received_email(cclft.contact_congress_letter, letter.formageddon_thread).deliver
-  end
-end
+/usr/local/bin/curl -sS 'http://www.opencongress.org/contact_congress_letters/get_replies' | /bin/awk '{ print strftime("\n%Y-%m-%d %H:%M:%S ::"), $0; }'

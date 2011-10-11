ac5baf502656900ae92feca325fec82a1a72de99
Andrew Ross <aross@opencongress.org>
2011-10-11 15:03:10 -0700

    updated yahoo geocoder (was using a deprecated version)

--- app/models/zipcode_district.rb
+++ app/models/zipcode_district.rb
@@ -1,12 +1,9 @@
 class ZipcodeDistrict < ActiveRecord::Base 
   def ZipcodeDistrict.zip_lookup(zip5, zip4 = nil)
     if zip4.blank?
-      find(:all, :select => "DISTINCT state, district", 
-           :conditions => ["zip5 = ?", zip5])
+      self.select("DISTINCT state, district").where(["zip5 = ?", zip5]).all
     else
-      find(:all, :select => "DISTINCT state, district", 
-           :conditions => ["zip5 = ? AND (zip4 = ? OR zip4 = 'xxxx')", zip5, zip4])
-
+      self.select("DISTINCT state, district").where(["zip5 = ? AND (zip4 = ? OR zip4 = 'xxxx')", zip5, zip4]).all
     end
   end
   

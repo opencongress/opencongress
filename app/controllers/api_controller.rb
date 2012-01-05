96947c5a91ecc3b353a1abb364b7e1090a5dd219
icedrake <github@mandrake.us>
2012-01-04 17:34:22 -0700

    output roll call votes from roll call api call

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -216,7 +216,7 @@ class ApiController < ApplicationController
   
   def bill_roll_calls
     bills = Bill.find_all_by_id(params[:bill_id])
-    do_render(bills, :except => [:current_support_pb, :support_count_1, :rolls, :hot_bill_category_id, :support_count_2, :vote_count_2], :include => [:roll_calls])    
+    do_render(bills, :except => [:current_support_pb, :support_count_1, :rolls, :hot_bill_category_id, :support_count_2, :vote_count_2], :include => {:roll_calls => {:include => :roll_call_votes}})    
   end
   
   def issues

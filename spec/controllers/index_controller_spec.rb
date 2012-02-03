b5cc959c27518e276ceea1f46d8169f0667dd381
Doug Cole <doug@estately.com>
2012-02-02 17:30:36 -0800

    first real spec!

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -7,5 +7,19 @@ describe IndexController do
       get :index
       response.should be_success
     end
+
+    it 'should load' do
+      Article.should_receive(:frontpage_gossip).and_return([
+        Article.new(
+          :title => 'Title',
+          :created_at => Time.now,
+          :excerpt => 'blah, blah..'
+        )
+      ])
+      get :index
+      response.should have_selector("strong.gossip") do |content|
+        content.text.should include('Title')
+      end
+    end
   end
 end

e8cfee8120f1afbe8e2a9a4a0cb60dd5e8d87753
Andrew Ross <aross@nindy.com>
2012-02-13 19:50:32 -0800

    added extra protection to production db

--- db/migrate/20111108013246_initial_schema.rb
+++ db/migrate/20111108013246_initial_schema.rb
@@ -1,11 +1,13 @@
 class InitialSchema < ActiveRecord::Migration
   def self.up
-    abcs = ActiveRecord::Base.configurations
-    ENV['PGHOST']     = abcs[Rails.env]["host"] if abcs[Rails.env]["host"]
-    ENV['PGPORT']     = abcs[Rails.env]["port"].to_s if abcs[Rails.env]["port"]
-    ENV['PGPASSWORD'] = abcs[Rails.env]["password"].to_s if abcs[Rails.env]["password"]
-    db = ActiveRecord::Base.configurations[Rails.env]['database']
-    system("psql -d #{db} -f #{Rails.root + 'db/migrate/initial_schema.sql'}")
+    unless Rails.env.production?
+      abcs = ActiveRecord::Base.configurations
+      ENV['PGHOST']     = abcs[Rails.env]["host"] if abcs[Rails.env]["host"]
+      ENV['PGPORT']     = abcs[Rails.env]["port"].to_s if abcs[Rails.env]["port"]
+      ENV['PGPASSWORD'] = abcs[Rails.env]["password"].to_s if abcs[Rails.env]["password"]
+      db = ActiveRecord::Base.configurations[Rails.env]['database']
+      system("psql -d #{db} -f #{Rails.root + 'db/migrate/initial_schema.sql'}")
+    end
   end
 
   def self.down
